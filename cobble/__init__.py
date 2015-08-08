a00e32659b3fbd7245684a375061c2fdb85006da
Michael Williamson <mike@zwobble.org>
2015-08-08 12:17:25 +0100

    Split out generation of the magic methods

--- cobble/__init__.py
+++ cobble/__init__.py
@@ -12,24 +12,36 @@ def data(cls):
         ),
         key=lambda field: field[1]._sort_key
     )
-    names = [name for name, field in fields]
-    assignments_source = "".join(
-        "\n    self.{0} = {0}".format(name)
-        for name in names
-    )
-    init_source = "def __init__(self, {0}):{1}\n".format(", ".join(names), assignments_source)
-    repr_source = "def __repr__(self):\n     return '{0}({1})'.format({2})\n".format(
-        cls.__name__,
-        ", ".join("{0}={{{1}}}".format(name, index) for index, name in enumerate(names)),
-        ", ".join("repr(self.{0})".format(name) for name in names)
-    )
     stash = {}
-    exec_(init_source + repr_source, globals(), stash)
+    exec_("\n".join(_magic_methods(cls, fields)), globals(), stash)
     for key, value in iteritems(stash):
         setattr(cls, key, value)
     
     return cls
 
+def _magic_methods(cls, fields):
+    return [
+        _make_init(fields),
+        _make_repr(cls, fields),
+    ]
+
+
+def _make_init(fields):
+    names = [name for name, field in fields]
+    assignments_source = "".join(
+        "\n    self.{0} = {0}".format(name)
+        for name in names
+    )
+    return "def __init__(self, {0}):{1}\n".format(", ".join(names), assignments_source)
+
+
+def _make_repr(cls, fields):
+    names = [name for name, field in fields]
+    return "def __repr__(self):\n     return '{0}({1})'.format({2})\n".format(
+        cls.__name__,
+        ", ".join("{0}={{{1}}}".format(name, index) for index, name in enumerate(names)),
+        ", ".join("repr(self.{0})".format(name) for name in names)
+    )
 
 _sort_key_count = itertools.count()
 
